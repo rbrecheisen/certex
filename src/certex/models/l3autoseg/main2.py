c4838c53c02e8de6b088c5f72aabc27035ff42f0
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-11-18 08:52:33 +0100

    Running validation by loading model from file

--- src/certex/models/l3autoseg/main2.py
+++ src/certex/models/l3autoseg/main2.py
@@ -91,18 +91,12 @@ def display(img_path, lab_path):
     plt.show()
 
 
-# curl -O https://thor.robots.ox.ac.uk/~vgg/data/pets/images.tar.gz
-# curl -O https://thor.robots.ox.ac.uk/~vgg/data/pets/annotations.tar.gz
-def main():
+def load_training_and_validation_data():
     img_paths = sorted([os.path.join(IMG_DIR, fname) for fname in os.listdir(IMG_DIR) if fname.endswith('.jpg')])
     lab_paths = sorted([os.path.join(LAB_DIR, fname) for fname in os.listdir(LAB_DIR) if fname.endswith('.png')])
     print(f'nr. samples: {len(img_paths)}')
     for img_path, lab_path in zip(img_paths[:10], lab_paths[:10]):
         print(img_path, '|', lab_path)
-    # display(img_paths[9], lab_paths[9])
-    tf.keras.backend.clear_session()
-    model = get_model(IMG_SIZE, NUM_CLASSES)
-    model.summary()
     val_samples = 1000
     random.Random(1337).shuffle(img_paths)
     random.Random(1337).shuffle(lab_paths)
@@ -110,21 +104,47 @@ def main():
     train_lab_paths = lab_paths[:-val_samples]
     validation_img_paths = img_paths[-val_samples:]
     validation_lab_paths = lab_paths[-val_samples:]
+    return train_img_paths, train_lab_paths, validation_img_paths, validation_lab_paths
+
+
+def display_mask(prediction):
+    mask = np.argmax(prediction, axis=1)
+    mask = np.expand_dims(mask, axis=1)
+    img = ImageOps.autocontrast(tf.keras.preprocessing.image.array_to_img(mask))
+    plt.figure(figsize=(15, 15))
+    plt.subplot(1, 2, 1)
+    plt.title('Input image')
+    plt.imshow(img)
+    plt.subplot(1, 2, 2)
+    plt.title('Predicted mask')
+    plt.imshow(mask)
+    plt.axis('off')
+    plt.show()
+
+
+# curl -O https://thor.robots.ox.ac.uk/~vgg/data/pets/images.tar.gz
+# curl -O https://thor.robots.ox.ac.uk/~vgg/data/pets/annotations.tar.gz
+def main():
+    # display(img_paths[9], lab_paths[9])
+    tf.keras.backend.clear_session()
+    train_img_paths, train_lab_paths, validation_img_paths, validation_lab_paths = load_training_and_validation_data()
     train_data = OxfordPets(BATCH_SIZE, IMG_SIZE, train_img_paths, train_lab_paths)
     validation_data = OxfordPets(BATCH_SIZE, IMG_SIZE, validation_img_paths, validation_lab_paths)
+    model = get_model(IMG_SIZE, NUM_CLASSES)
+    model.summary()
     model.compile(loss='sparse_categorical_crossentropy')  # optimizer=rmsprop
     callbacks = [tf.keras.callbacks.ModelCheckpoint('oxford_seg.h5', save_best_only=True)]
-    epochs = 15
-    model.fit(train_data, epochs=epochs, validation_data=validation_data, callbacks=callbacks)
+    model.fit(train_data, epochs=15, validation_data=validation_data, callbacks=callbacks)
     model.save(MOD_DIR)
     model.save_weights(MOD_DIR)
 
 
-def validate(validation_img_paths, validation_lab_paths):
-    model = tf.keras.models.load_model(MOD_DIR)
+def validate():
+    _, _, validation_img_paths, validation_lab_paths = load_training_and_validation_data()
     validation_data = OxfordPets(BATCH_SIZE, IMG_SIZE, validation_img_paths, validation_lab_paths)
+    model = tf.keras.models.load_model(MOD_DIR)
     validation_predictions = model.predict(validation_data)
-    print(validation_predictions)
+    display_mask(validation_predictions[10])
 
 
 if __name__ == '__main__':
